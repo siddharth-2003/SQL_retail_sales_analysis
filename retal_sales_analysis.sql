ed7e26f9f3541c07c3c672eaa9cac88e753f64f2
trexxgithub <119318701+trexxgithub@users.noreply.github.com>
2026-04-24 12:13:18 +0530

    Add files via upload

---
+++ retal_sales_analysis.sql
@@ -0,0 +1,152 @@
+--creating table
+/* DROP TABLE IF EXISTS retail_sales_tb;
+CREATE TABLE retail_sales_tb
+	( 
+		transactions_id INT PRIMARY KEY,
+		sale_date DATE,
+		sale_time TIME,
+		customer_id INT,
+		gender VARCHAR(15),
+		age INT,
+		category VARCHAR(15),
+		quantity INT,
+		price_per_unit FLOAT,
+		cogs FLOAT,
+		total_sale FLOAT
+	); */
+-- data cleaning
+
+/* select * from retail_sales_tb
+where 
+	 transactions_id IS NULL
+	 or
+	 sale_date is null
+	 or
+	 sale_time is null
+	 or
+	 customer_id is null
+	 or
+	 gender is null
+	 or 
+	 age is null
+	 or
+	 category is null
+	 or
+	 quantity is null
+	 or
+	 price_per_unit is null
+	 or
+	 cogs is null
+	 or
+	 total_sale is null */
+/* delete from  retail_sales_tb
+where 
+	 transactions_id IS NULL
+	 or
+	 sale_date is null
+	 or
+	 sale_time is null
+	 or
+	 customer_id is null
+	 or
+	 gender is null
+	 or 
+	 age is null
+	 or
+	 category is null
+	 or
+	 quantity is null
+	 or
+	 price_per_unit is null
+	 or
+	 cogs is null
+	 or
+	 total_sale is null
+*/
+
+--data exploration
+--How many sales do we have?
+
+select count(*) as total_sales from retail_sales_tb;
+
+-- How many customer do we have? 
+
+select count(distinct customer_id) as total_customers
+from retail_sales_tb;
+
+--How many categories do we have
+
+select distinct category from retail_sales_tb;
+
+-- Data Analysis & Key Business problems & Answers
+-- Q1) Write a SQL query to retrieve all columns for sales made on '2022-11-05:
+select * from retail_sales_tb
+where sale_date = '2022-11-05';
+-- Q2) Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022:
+select *
+from retail_sales_tb 
+where 
+	category = 'Clothing'
+	and TO_CHAR(sale_date,'YYYY-MM') = '2022-11'
+	and quantity >=4;
+-- Q3) Write a SQL query to calculate the total sales (total_sale) for each category.:
+select category, sum(total_sale) as total_sales, count(*) as total_orders
+from retail_sales_tb
+group by 1;
+-- Q4) Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.:
+select category,round(avg(age)) as average_age
+from retail_sales_tb
+group by 1
+having category = 'Beauty';
+--Q5) Write a SQL query to find all transactions where the total_sale is greater than 1000.:
+select *
+from retail_sales_tb
+where total_Sale>1000;
+--Q6) Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.:
+select category,gender,sum(transactions_id) as total_transactions
+from retail_sales_tb
+group by category,gender
+order by 1;
+--Q7) Write a SQL query to calculate the average sale for each month. Find out best selling month in each year:
+select *
+from(
+    select
+	extract(year from sale_date) as year,
+	extract(month from sale_date) as month, 
+	round(avg(total_sale)) as average_sale,
+	rank() over(partition by extract(year from sale_date) order by avg(total_sale) desc) as rank
+   from retail_sales_tb
+   group by 1,2) 
+as t1 
+where rank = 1;
+--Q8) Write a SQL query to find the top 5 customers based on the highest total sales
+select customer_id,round(sum(total_sale))
+		from retail_sales_tb
+		group by 1
+		order by 2 desc
+		limit 5;
+--Q9) Write a SQL query to find the number of unique customers who purchased items from each category.:
+select count(distinct customer_id) as distinct_customers, category
+from retail_sales_tb 
+group by category;
+--Q10) Write a SQL query to create each shift and number of orders (Example Morning <12, Afternoon Between 12 & 17, Evening >17):
+with hourly_sales
+as
+(
+select *, 
+		case 
+			when extract(hour from sale_time)<12 then 'Morning'
+			when extract(hour from sale_time) between 12 and 17 then 'Afternoon'
+			else 'Evening'
+		end as shift
+from retail_sales_tb
+)
+select 
+	shift,
+	count(*) as total_orders
+from hourly_sales
+group by shift
+
+--End of project
+		
+	
